096aac57b6217d1f073cc4117397b4b7ae8deb17
SergioEstanRuiz <sergioestanruiz@gmail.com>
2025-01-17 17:33:27 +0000

    Empirical distribution of errors computed.

--- MainFile.r
+++ MainFile.r
@@ -3,21 +3,40 @@ library("igraph")
 
 # Generate data from a random graph and GNAR
 graph <- make_graph(edges = c(1, 2, 2, 3, 2, 4, 4, 5), n = 5, directed = FALSE) 
-matrix <- as_adjacency_matrix(graph, type = "upper")
-data <- GNARsim(n=200, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5,0.3)))
+realMatrix <- as_adjacency_matrix(graph, type = "upper")
+data <- GNARsim(n=200, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5)))
+data.ts <- ts(data)
 
-errors <- c(0)
-for (i in 0:(2^10-1)){
-    # Convert i to binary
-    binary <- as.integer(intToBits(i))
-    binary <- binary[1:10]
-    # Convert binary to a matrix
-    matrix <- matrix(0,5,5)
-    matrix[upper.tri(matrix)] <- binary
-    # Convert matrix to a graph
-    net <- matrixtoGNAR(matrix)
-    # Fit GNAR
-    fit <- GNARfit(data[], net, alphaOrder = )
-    # Calculate error
-    errors <- c(errors, fit$error)
+generateError <- function(size = 5){
+    graph <- erdos.renyi.game(size, p=0.3, type = "gnp")
+    data <- GNARsim(n=200, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5)))
+    data.ts <- ts(data)
+    errors <- c(0)
+    numberofGraphs <- 2^(size*(size-1)/2)-1
+    for (i in 1:numberofGraphs){
+        # Convert i to binary
+        binary <- as.integer(intToBits(i))
+        binary <- binary[1:10]
+        # Convert binary to a matrix
+        matrix <- matrix(0,size,size)
+        matrix[upper.tri(matrix)] <- binary
+        # Convert matrix to a graph
+        net <- matrixtoGNAR(matrix)
+        # Fit GNAR
+        fit <- GNARfit(vts = data.ts[1:199,], net = net, alphaOrder = 1, betaOrder = c(1))
+        # Calculate error
+        err <- sum((data[200] - predict(fit))^2)
+        errors <- c(errors, err)
+    }
+        return(errors)
 }
+
+errors <- generateError(size=5)
+
+pdf(file="./testFile.pdf")
+# plot histogram of errors 
+hist(errors, breaks = 100, col = "lightblue", border = "black", xlab = "Error", main = "Histogram of Errors")
+# add description
+mtext("Histogram representing distribution of errors for data \n simulated from a GNAR(1,[1]) for a 5-node network", side=3)
+dev.off()
+
