b620af7eb7759bf5c4799cffdc6bbfea5d07b6a7
SergioEstanRuiz <sergioestanruiz@gmail.com>
2025-01-29 17:35:00 +0000

    - Deleted all histograms, density plots. Small k irrelevant

--- MainFile.r
+++ MainFile.r
@@ -1,15 +1,10 @@
 library("GNAR")
 library("igraph")
 
-# Generate data from a random graph and GNAR
-graph <- make_graph(edges = c(1, 2, 2, 3, 2, 4, 4, 5), n = 5, directed = FALSE) 
-realMatrix <- as_adjacency_matrix(graph, type = "upper")
-data <- GNARsim(n=200, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5)))
-data.ts <- ts(data)
-
 generateError <- function(size = 5, k = 1, seed = NA){
     if (!is.na(seed)){
         set.seed(seed)
+        print("Seed is set")
     }
     graph <- erdos.renyi.game(size, p=0.3, type = "gnp")
     data <- GNARsim(n=200, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5)))
@@ -39,7 +34,7 @@ generateErrorAll <- function(size = 5, k = 10, seed = NA){
         set.seed(seed)
     }
     graph <- erdos.renyi.game(size, p=0.3, type = "gnp")
-    data <- GNARsim(n=200, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5)))
+    data <- GNARsim(n=250, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5)))
     data.ts <- ts(data)
     numberofGraphs <- 2^(size*(size-1)/2)-1
     errors <- matrix(0, k, numberofGraphs)
@@ -54,8 +49,8 @@ generateErrorAll <- function(size = 5, k = 10, seed = NA){
         err <- 0
         net <- matrixtoGNAR(matrix)
         for (j in 1:k){
-            fit <- GNARfit(vts = data.ts[j:(199-k+j),], net = net, alphaOrder = 1, betaOrder = c(1))
-            err <- err + sum((data[(200-k+j),] - predict(fit))^2)
+            fit <- GNARfit(vts = data.ts[j:(249-k+j),], net = net, alphaOrder = 1, betaOrder = c(1))
+            err <- err + sum((data[(250-k+j),] - predict(fit))^2)
             errors[j,i] <- err
         }
     }
@@ -64,46 +59,29 @@ generateErrorAll <- function(size = 5, k = 10, seed = NA){
     }
         return(errors)
 }
-plotting <- function(x, mean, std, xlab = "x-axis", ylab = "y-axis", title = "Title of Plot"){
-    
-    set.seed(1234)
-    df <- data.frame(x =x, F = mean, L = mean - std, U = mean + std)
 
-    upperlim <- max(mean) + 1.5*max(std)
-    lowerlim <- min(mean) - 1.5*max(std)
+# set.seed(1234)
+# graph <- erdos.renyi.game(5, p=0.3, type = "gnp")
+# data <- GNARsim(n=250, net = igraphtoGNAR(graph), alphaParams = list(c(rep(0.2,5))), betaParams = list(c(0.5)))
+# data.ts <- ts(data)
+# net <- igraphtoGNAR(graph)
+# err <- 0
+# for (j in 1:40){
+#     fit <- GNARfit(vts = data.ts[j:(249-40+j),], net = net, alphaOrder = 1, betaOrder = c(1))
+#     err <- err + sum((data[(250-40+j),] - predict(fit))^2)
+# }
+# err <- err/40
+# summary(as.numeric(errorsAll[40,]))
 
-    plot(df$x, df$F, ylim = c(lowerlim, upperlim), type = "l", xlab = xlab, ylab = ylab, main = title)
-    #make polygon where coordinates start with lower limit and 
-    # then upper limit in reverse order
-    polygon(c(df$x,rev(df$x)),c(df$L,rev(df$U)),col = "grey75", border = FALSE)
-    lines(df$x, df$F, lwd = 2)
-    #add red lines on borders of polygon
-    lines(df$x, df$U, col="red",lty=2)
-    lines(df$x, df$L, col="red",lty=2)
-}
+error <- generateError(size=5, k=40, seed = 1234)
+write.table(error, file = "./error_n=5_k=40.txt", sep = "\t")
+error <- read.table("./error_n=5_k=40.txt", sep = "\t")
 
-errors1 <- generateError(size=5, k=2, seed= 1234)
-errors2 <- generateError(size=5, k=2, seed = 1234) # good one
-print(errors1-errors2)
-print(errors2- errorsAll[2,])
-errorsAll <- generateErrorAll(size=5, k=10, seed = 1234)
-x <- 1:10
-mean <- apply(errorsAll, 1, mean)
-std <- apply(errorsAll, 1, sd)
 
-pdf(file="./histogram_n=5_k=5to10.pdf")
-colors <- rainbow(5)
-plot(NULL, xlim = c(4, 5.2), ylim = c(0, 10), xlab = "Error", ylab = "Density", main = "Density Plots of Errors for Each k")
-for (i in 1:5) {
-    lines(density(errorsAll[(i+5),]), col = colors[i], lwd = 2)
-}
-legend("topright", legend = paste("k =", 6:10), col = colors, lwd = 2)
-dev.off()
 
-pdf(file="./histogram_n=5_k=2.pdf")
+pdf(file="./histogram_n=5_k=40_part2.pdf")
 # plot histogram of errors 
-hist(errors, breaks = 100, col = "lightblue", border = "black", xlab = "Error", main = "Histogram of Errors")
-# add description
-mtext("Histogram representing distribution of errors for data \n simulated from a GNAR(1,[1]) for a 5-node network, k=1", side=3)
+hist(as.numeric(error), breaks = 100, col = "lightblue", border = "black", xlab = "Error", main = "Histogram of Errors")
+mtext("Histogram representing distribution of errors for data \n simulated from a GNAR(1,[1]) for a 5-node network, k=40", side=3)
 dev.off()
 
